d95f0fc05d12b4cf9c3bfc05132e64d16234cd1f
nhamlh <lehoainham@gmail.com>
2015-07-28 23:25:59 +0700

    add exception classes for telnet connection

---
+++ lib/connection_exceptions.py
@@ -0,0 +1,24 @@
+from exceptions import Exception
+
+
+class TelnetConnectionError(Exception):
+    def __init_(self):
+        pass
+
+    def __str__(self):
+        return
+
+
+class TelnetAuthenticationError(Exception):
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return
+
+class TelnetEnableError(Exception):
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return
