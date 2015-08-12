2af032ec28b954cef418448389886b2882dd9189
NhamLH <lehoainham@gmail.com>
2015-08-12 17:24:07 +0700

    update config.py

--- nbackup/config.py
+++ nbackup/config.py
@@ -1,5 +1,9 @@
+from os import path
+
 DEBUG = True
 
 SECRET_KEY = 'verylongandsecuresecretkey'
 
 SQLALCHEMY_DATABASE_URI = 'postgresql://nbackup:password@localhost/nbackup'
+
+_BASE_DIR_ = path.dirname(path.abspath(__file__))
